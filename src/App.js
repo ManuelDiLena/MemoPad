addf3ebfddecf6e30c3c51dd84769bb06e7f8d40
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-07-04 18:02:11 +0200

    Object iteration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const [items, setItems] = useState([{
+        id: 0,
+        title: 'Primer nota',
+        text: '# HolaMundo',
+        pinned: false,
+        created: Date.now()
+    }]);
+
     return (
         <div className="App container">
             <div className='panel'>
                 <div className='menu'>
-                    <div className='list'>
-
-                    </div>
+                    <input className='search' placeholder='Search'/>
+                    <button className='btn'>New Memo</button>
+                </div>
+                <div className='list'>
+                    {
+                        items.map((item, i) => {
+                            return <div key={item.id}>{item.title}</div>
+                        })
+                    }
                 </div>
             </div>
 
             <div>
-                <div className='editor'>
-                    <div className='preview'>
-                        
-                    </div>
-                </div>
+                <div className='editor'></div>
+                <div className='preview'></div>
             </div>
         </div>
     );
