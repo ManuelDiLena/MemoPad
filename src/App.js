89c0cc14315d081d7acdbc4de906f1f55c5abb98
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-07-04 17:41:51 +0200

    Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App container">
+            <div className='panel'>
+                <div className='menu'>
+                    <div className='list'>
+
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className='editor'>
+                    <div className='preview'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
